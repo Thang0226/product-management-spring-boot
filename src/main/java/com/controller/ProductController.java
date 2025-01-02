2ad89f6ee8f40449a623108bbafd48485315875f
Thang0226 <thang.nd0226@gmail.com>
2025-01-02 21:45:47 +0700

    can add new product & show all products

---
+++ src/main/java/com/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.controller;
+
+import com.model.Product;
+import com.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private IProductService productService;
+
+    @GetMapping
+    public String listProducts(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return "list";
+    }
+
+    @GetMapping("/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(Product product, RedirectAttributes redirectAttributes) {
+        productService.save(product);
+        redirectAttributes.addFlashAttribute("message", "New Product added successfully");
+        return "redirect:/products";
+    }
+}
---
+++ src/main/java/com/model/Product.java
@@ -0,0 +1,18 @@
+package com.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="product")
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    String name;
+    String description;
+    String manufacturer;
+    double price;
+}
